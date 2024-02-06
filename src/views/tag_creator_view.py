fc02061f0791ecbe10385cf6464544fa5585536b
Salomao S <souza95salomao@gmail.com>
2024-02-06 17:53:24 -0400

    implemented barcode generation

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     def validate_request(self, http_request_body: Dict):
@@ -14,6 +15,7 @@ class TagCreatorView:
             return HttpResponse(status_code=400, body={ "success":"false" })
 
         product_code = request_body['product_code']
-        print({ 'product_code': product_code })
+        tg_creator = TagCreatorController()
+        response_body = tg_creator.create(product_code)
 
-        return HttpResponse(status_code=201, body={ "success":"true" })
+        return HttpResponse(status_code=201, body=response_body)
