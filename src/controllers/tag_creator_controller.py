d52d940bc65224ed2611f1da1db0b953ac1c013f
SalomaoS <souza95salomao@gmail.com>
2024-02-16 16:26:41 +0000

    refactor controllers to be more generic

---
+++ src/controllers/tag_creator_controller.py
@@ -0,0 +1,20 @@
+from typing import Dict
+from src.drivers.tag_creator_handler import TagCreatorHandler
+
+
+class TagCreatorController:
+    def __init__(self, handler:TagCreatorHandler):
+        self.tag_creator = handler
+
+
+    def create(self, product_data:str, extension:str) -> Dict:
+        tag_path = self.tag_creator.create_tag(product_data, extension)
+        return self.__format_response__(tag_path, extension)
+
+
+    def __format_response__(self,tag_path:str,extension:str) -> Dict:
+        return {
+            'type': 'x-image-tag',
+            'extension': extension,
+            'path': f'{tag_path}.{extension.lower()}'
+        }
