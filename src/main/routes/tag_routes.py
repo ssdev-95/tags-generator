2acf49bb80d62dbc7d7ca6f0c7887d94737bfcde
SalomaoS <souza95salomao@gmail.com>
2024-02-16 16:28:14 +0000

    refactor routes to treat request based on tag type

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,44 +1,24 @@
 from flask import Blueprint, request, jsonify
-from src.views.http_types.http_request import HttpRequest
-from src.errors.error_handlers import handle_errors
 from src.validators.tag_creator_validator import TagCreatorValidator
-from src.views.barcode_creator_view import BarcodeCreatorView
-from src.views.qrcode_creator_view import QrcodeCreatorView
+from src.views.http_types.http_request import HttpRequest
+from src.views.tag_creator_view import TagCreatorView
 
 
-tag_routes_bp = Blueprint('tag_routes', __name__)
+tags_bp = Blueprint('tag_routes',__name__)
 
 
-@tag_routes_bp.route('/tags/barcode/create', methods=['POST'])
-def create_barcode_tag():
-    allowed_formats = ['SVG','PNG','JPEG','ICO']
-    http_response = None
-
-    try:
-        tc_validator = TagCreatorValidator(allowed_formats)
-        tc_validator.validate(request)
-
-        tag_creator = BarcodeCreatorView()
-        http_request = HttpRequest(body=request.json)
-        http_response = tag_creator.create_tag(http_request)
-    except Exception as err:
-        http_response = handle_errors(err)
-
-    return jsonify(http_response.body), http_response.status_code
-
-
-@tag_routes_bp.route('/tags/qrcode/create', methods=['POST'])
-def create_qrcode_tag():
+@tags_bp.route('/tags/create', methods=['POST'])
+def createTag():
+    tag_type = request.args.get('type', 'BARCODE')
     allowed_formats = ['SVG','PNG']
-    http_response = None
 
-    try:
-        tc_validator = TagCreatorValidator(allowed_formats)
-        tc_validator.validate(request)
-        tag_creator = QrcodeCreatorView()
-        http_request = HttpRequest(body=request.json)
-        http_response = tag_creator.create_tag(http_request)
-    except Exception as err:
-        http_response = handle_errors(err)
+    if tag_type == 'BARCODE':
+        allowed_formats = allowed_formats + ['JPEG','ICO']
 
+    tc_validator = TagCreatorValidator(allowed_formats)
+    tc_validator.validate(request)
+
+    http_request = HttpRequest(body=request.json)
+    tag_creator_view = TagCreatorView(tag_type)
+    http_response = tag_creator_view.create_tag(http_request)
     return jsonify(http_response.body), http_response.status_code
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from src.main.routes.tag_routes import tag_routes_bp
+from src.main.routes.tag_routes import tags_bp
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(tag_routes_bp)
+    app.register_blueprint(tags_bp)
 
     return app
